6cb58f6c6d51c8b75452e4049cc34197cd265ba6
texeng001 <komatsu.haruka.sb@tex.nipponsteel.com>
2024-10-23 14:29:51 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'WildRydes', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '62qkes52f9vccs2pqpv2gu3dmh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
